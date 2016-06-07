bd70df31b4f6258330e07942bdda1b8188ef7a28
brenton thomas <brenton.thomas@evilwicked.com>
2016-06-08 09:30:53 +1000

    rnamed internal threadpool functions

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['calc',['calc',['../classevil_1_1_thread_pool.html#a520e6d58a1d2c2d288b5ea689045d6a4',1,'evil::ThreadPool::calc(F &amp;&amp;func, Args &amp;&amp;...args)'],['../classevil_1_1_thread_pool.html#a9f6348becd9f747f9fc542ac6c6f0a4c',1,'evil::ThreadPool::calc(C &amp;&amp;cinst, F &amp;&amp;func, Args &amp;&amp;...args)'],['../namespaceevil_1_1thread__pool.html#ad672834ea354a2becb4a5073143d75be',1,'evil::thread_pool::calc(F &amp;&amp;func, Args &amp;&amp;...args)'],['../namespaceevil_1_1thread__pool.html#aeb47443969e0beae1968c158cf712a1c',1,'evil::thread_pool::calc(C &amp;&amp;cinst, F &amp;&amp;func, Args &amp;&amp;...args)']]],
   ['clear',['clear',['../classevil_1_1_test_data_table.html#ab5f6dadc3bf96f667d22a5dd4f17bc39',1,'evil::TestDataTable::clear()'],['../classevil_1_1_thread_queue.html#afff44943b41a6759cae74f456e01287f',1,'evil::ThreadQueue::clear()']]],
   ['cols',['Cols',['../classevil_1_1_test_data_table.html#abf0b9fd443057491433ce5fb2d194783',1,'evil::TestDataTable']]],
   ['connectsignal',['connectSignal',['../classevil_1_1_slot_base.html#a933c938d390e31d9d55ce20d6885a82f',1,'evil::SlotBase']]]
